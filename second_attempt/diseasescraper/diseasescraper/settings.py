e35cdc16169924972465c7ce6df6398c0e6a88d6
Jozi-Mwangi <mwasjozi@gmail.com>
2023-09-19 14:38:01 +0300

    Hosp changes

--- second_attempt/diseasescraper/diseasescraper/settings.py
+++ second_attempt/diseasescraper/diseasescraper/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = "diseasescraper.spiders"
 #USER_AGENT = "diseasescraper (+http://www.yourdomain.com)"
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
--- second_attempt/diseasescraper/diseasescraper/spiders/diseasespider.py
+++ second_attempt/diseasescraper/diseasescraper/spiders/diseasespider.py
@@ -3,8 +3,12 @@ import scrapy
 
 class DiseasespiderSpider(scrapy.Spider):
     name = "diseasespider"
-    allowed_domains = ["dph.illinois.gov"]
-    start_urls = ["https://dph.illinois.gov/topics-services/diseases-and-conditions/diseases-a-z-list.html"]
+    allowed_domains = ["https://www.health.harvard.edu/a-through-c"]
+    start_urls = ["https://www.health.harvard.edu/a-through-c"]
 
     def parse(self, response):
+        diseases = response.xpath("//p/strong")
+        descriptions = response.xpath('//p/text()')
+
+        
         pass
